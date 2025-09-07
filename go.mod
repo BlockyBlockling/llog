7b3449d7a1578da8bad5e3389807f631250c6591
Blocky Blockling <blockyblockling@limecode.de>
2025-09-07 12:46:21 +0200

    Adding base functionality (initial commit)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blockyblockling/llog
+
+go 1.24.6
---
+++ main.go
@@ -0,0 +1,155 @@
+package llog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Level int
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
+var levelName = map[Level]string{
+	LevelDebug: "Debug",
+	LevelInfo:  "Info",
+	LevelWarn:  "Warn",
+	LevelError: "Error",
+	LevelFatal: "Fatal",
+}
+
+var levelNameFormatted = map[Level]string{
+	LevelDebug: bold + string(Blue) + "DEBU" + reset,
+}
+
+type Color string
+
+const (
+	reset = "\033[0m"
+	bold  = "\033[1m"
+
+	Black        Color = "\033[30m"
+	Red          Color = "\033[31m"
+	Green        Color = "\033[32m"
+	Yellow       Color = "\033[33m"
+	Blue         Color = "\033[34m"
+	Magenta      Color = "\033[35m"
+	Cyan         Color = "\033[36m"
+	LightGray    Color = "\033[37m"
+	DarkGray     Color = "\033[90m"
+	LightRed     Color = "\033[91m"
+	LightGreen   Color = "\033[92m"
+	LightYellow  Color = "\033[93m"
+	LightBlue    Color = "\033[94m"
+	LightMagenta Color = "\033[95m"
+	LightCyan    Color = "\033[96m"
+	White        Color = "\033[97m"
+)
+
+//TODO: Add any on the messaging arguments and not only string
+//TODO: Improve Logging
+
+func Debug(msg string) {
+	printStdout(
+		timestamp(),
+		" ",
+		levelNameFormatted[LevelDebug],
+		" ",
+		msg,
+		"\n",
+	)
+}
+
+func DebugWithStack(msg string) {
+	printStdout(
+		timestamp(),
+		" ",
+		levelNameFormatted[LevelDebug],
+		" ",
+		stackLoc(2),
+		" ",
+		msg,
+		"\n",
+	)
+}
+
+func Info(msg string) {
+	printStdout(
+		timestamp(),
+		" ",
+		levelNameFormatted[LevelInfo],
+		" ",
+		msg,
+		"\n",
+	)
+}
+
+func Error(msg string) {
+	printStdout(
+		timestamp(),
+		" ",
+		levelNameFormatted[LevelError],
+		" ",
+		stackLoc(2),
+		" ",
+		Red,
+		msg,
+		reset,
+		"\n",
+	)
+}
+
+// Recieve an Error with a possible Nil value. It will only log if err != nil
+// TODO: Add an optional attribute to add custom messages to the error
+func ErrNil(err error) (errNotNil bool) {
+	if err != nil {
+
+		printStdout(
+			timestamp(),
+			" ",
+			levelNameFormatted[LevelError],
+			" ",
+			stackLoc(2),
+			" ",
+			Red,
+			err.Error(),
+			reset,
+			"\n",
+		)
+
+		return true
+	}
+	return false
+}
+
+// TODO: Add an argument adding spaces between components
+func printStdout(components ...any) {
+	//Printing to Stdout
+	_, err := fmt.Fprint(os.Stdout, components...)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func timestamp() string {
+	return string(DarkGray) + time.Now().Format("2006/01/02 15:04:05")
+}
+
+func stackLoc(skip int) string {
+	cwd, _ := os.Getwd()
+	cwd += "/"
+	_, file, line, _ := runtime.Caller(skip)
+	fileLocal := strings.TrimPrefix(file, cwd)
+
+	return string(DarkGray) + fileLocal + ":" + strconv.Itoa(line)
+}
