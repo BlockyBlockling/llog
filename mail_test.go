0bbbe23bc460551cf75ea17784939e8fdff4f3d7
Blocky Blockling <blockyblockling@limecode.de>
2026-05-03 23:10:36 +0200

    Adding environment based mail testing config

---
+++ mail_test.go
@@ -0,0 +1,31 @@
+package llog
+
+import (
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestMail(t *testing.T) {
+	//Running Mail Test
+	t.Log("Running Mail Test:")
+
+	TEST_MAIL_ADDRESS := os.Getenv("TEST_MAIL_ADDRESS")
+	TEST_MAIL_TARGET := os.Getenv("TEST_MAIL_TARGET")
+	TEST_MAIL_URL := os.Getenv("TEST_MAIL_URL")
+	TEST_MAIL_PORT_STRING := os.Getenv("TEST_MAIL_PORT")
+	TEST_MAIL_PORT, err := strconv.Atoi(TEST_MAIL_PORT_STRING)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	TEST_MAIL_USERNAME := os.Getenv("TEST_MAIL_USERNAME")
+	TEST_MAIL_PASSWORD := os.Getenv("TEST_MAIL_PASSWORD")
+
+	InitMail(TEST_MAIL_ADDRESS, []string{TEST_MAIL_TARGET}, TEST_MAIL_URL, TEST_MAIL_PORT, TEST_MAIL_USERNAME, TEST_MAIL_PASSWORD, "Automatic llog Testing")
+	err = NotifyMail("Test Notify")
+	if err != nil {
+		t.Error("Mail Send Error: " + err.Error())
+		t.Fail()
+	}
+}
